4f38518f87ef301179a189d3c015726f3424f7d8
oliphant <oliphant@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-11-03 22:01:22 +0000

    Remove old docs.  Move enstaller out of enthought name-space.  Add patches to temporary directory.

---
+++ enstaller_setup.py
@@ -0,0 +1,11 @@
+from distutils import setup
+
+setup(
+    name = 'Enstaller',
+    version = '3.0.0',
+    description  = 'Enthought install tools',
+    author       = 'Enthought, Inc',
+    author_email = 'info@enthought.com',
+    url          = 'http://code.enthought.com/ets',
+    license      = 'BSD',
+)
