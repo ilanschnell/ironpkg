47c2f619e5664ab21b4eb4f130f153f3e8bdfdbd
ischnell <ischnell@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-11-29 02:00:42 +0000

    Added version option to enpgk in Enstaller.

--- enstaller/cmdline.py
+++ enstaller/cmdline.py
@@ -27,12 +27,23 @@ from utilities import remove_eggs_from_path, rst_table, get_platform
 from upgrade import upgrade
 from proxy.api import setup_proxy
 
+
+try:
+    from enstaller import __version__
+except ImportError:
+    from __init__ import __version__
+
+
 # set up global variables
-PLAT, PLAT_VER = get_platform() 
-ENTHOUGHT_REPO = ("http://code.enthought.com/enstaller/eggs/%s/%s" %
+PLAT, PLAT_VER = get_platform()
+
+ENTHOUGHT_REPO = (
+    "http://code.enthought.com/enstaller/eggs/%s/%s" % (PLAT, PLAT_VER))
+
+ENTHOUGHT_UNSTABLE_REPO = (
+    "http://code.enthought.com/enstaller/eggs/%s/%s/unstable" %
     (PLAT, PLAT_VER))
-ENTHOUGHT_UNSTABLE_REPO = ("http://code.enthought.com/enstaller/eggs/%s/%s/unstable" %
-    (PLAT, PLAT_VER))
+
 PYPI_REPO = "http://pypi.python.org/simple"
 
 
@@ -46,20 +57,23 @@ def get_local_repos():
             repos.append(repo)
     return repos
 
+
 def get_site_packages():
     """Find the site-packages directory
     """
     # is there a better way than this?
-    site_packages = [path for path in sys.path if path.endswith('site-packages')]
+    site_packages = [path for path in sys.path
+                     if path.endswith('site-packages')]
     if site_packages:
         return EasyInstallRepository(location=site_packages[0])
     else:
         error("Can't locate site-packages directory in path.")
 
+
 def query_user(msg, default=""):
     """Present a yes/no question to the user and return a response
     """
-    
+
     if default:
         msg += "[%s] " % default
     response = ""
@@ -68,6 +82,7 @@ def query_user(msg, default=""):
         if response == "" and default:
             response = default
     return response[0] == "y"
+
 
 def user_select(header, data, prompt, default="1", max_width=0):
     """Present a collection of options to the user and return a response
@@ -91,6 +106,7 @@ def user_select(header, data, prompt, default="1", max_width=0):
         return int(response)-1
     else:
         return None
+
 
 def upgrade_project(keys, local_repos=None, remote_repos=None, interactive=True,
     dry_run=False, term_width=0):
@@ -118,9 +134,9 @@ def upgrade_project(keys, local_repos=None, remote_repos=None, interactive=True,
             else:
                 requirement = Requirement.parse(project)
         requirements.append(requirement)
-    
+
     print requirements
-    
+
     install_requirement(requirements, local_repos=local_repos,
         remote_repos=remote_repos, interactive=interactive, dry_run=dry_run,
         term_width=term_width)
@@ -140,7 +156,7 @@ def install_requirement(requirements, target_repo=None, local_repos=None,
         local_repos = get_local_repos()
     local = RepositoryUnion(get_local_repos())
     available = RepositoryUnion(local_repos+remote_repos)
-    
+
     # generate proposals
     installed = dict((key, project.active_package)
                      for key, project in local.projects.items()
@@ -152,40 +168,41 @@ def install_requirement(requirements, target_repo=None, local_repos=None,
                 if package.distribution in requirement]
         if not packages:
             warning("Could not find a package which matches requirement %s" %
-                  requirement)
+                    requirement)
             continue
         if interactive and len(packages) > 1:
             selection = user_select(["version", "active", "location"],
                 [pkg.metadata for pkg in packages], "Select package: ",
                 max_width=term_width)
-            
+
             #selection = user_select(["Package '%s' at %s%s" % (pkg.name,
             #    pkg.location, " (Active)" if pkg.active else "")
             #    for pkg in packages], "Select package: ")
             if selection == None:
-                info("User selected no package for requirement %s" % requirement)
+                info("User selected no package for requirement %s" %
+                     requirement)
                 continue
             package = packages[selection]
         else:
             package = packages[0]
-        
+
         if package.active:
             # nothing to do
             info("Package %s satisfies %s and is already active" %
                 (package.name, requirement))
         else:
             to_install.append(package)
-    
+
     if not to_install:
         return
-    
+
     upgrades = upgrade(to_install, installed, available)
     try:
         proposal, reasoning = upgrades.next()
     except StopIteration:
         info("Unable to create a consistent installation plan.")
         return
-    
+
     if interactive:
         response = False
         while not response:
@@ -200,12 +217,13 @@ def install_requirement(requirements, target_repo=None, local_repos=None,
                         break
                 else:
                     active_project = None
-                
+
                 if active_project is None:
                     print ("  Install %s from %s" % (package.name,
                         package.location))[:term_width]
                 else:
-                    print ("  Upgrade %s from %s to %s from %s" % (active_project.name,
+                    print ("  Upgrade %s from %s to %s from %s" % (
+                        active_project.name,
                         active_project.active_package.version, package.version,
                         package.location))[:term_width]
             response = query_user("Accept proposed installation plan (y/n)? ",
@@ -214,9 +232,10 @@ def install_requirement(requirements, target_repo=None, local_repos=None,
                 try:
                     proposal, reasoning = upgrades.next()
                 except StopIteration:
-                    info("No proposed installation plan was acceptable to the user.")
+                    info("No proposed installation plan was acceptable "
+                         "to the user.")
                     return
-    
+
     # first activate any local packages
     active_environments = set()
     for key, package in proposal.items():
@@ -228,11 +247,12 @@ def install_requirement(requirements, target_repo=None, local_repos=None,
             env.save()
         else:
             print "Saving .pth file."
-    
+
     for key, package in proposal.items():
         if isinstance(package, RemotePackage):
             package.install(target_repo, dry_run=dry_run)
-       
+
+
 def remove_requirement(requirements, repos=None, interactive=False,
         dry_run=False):
     """Remove all installed packages which match the requirements
@@ -252,14 +272,16 @@ def remove_requirement(requirements, repos=None, interactive=False,
                                 key=lambda x: x.name):
                             print "Package %s depends on %s" % (dep.name,
                                 package.name)
-                        response = query_user("Remove '%s' from %s (y/n)? " % (package.name,
-                            package.repository.location), default="y")
+                        response = query_user("Remove '%s' from %s (y/n)? " %
+                                              (package.name,
+                                               package.repository.location),
+                                              default="y")
                         if not response:
                             continue
                     package.remove(dry_run=dry_run)
         if not removed:
             warning("Found no requirement which matches %s" % requirement)
- 
+
 def deactivate_requirement(requirements, repos=None, interactive=False,
         dry_run=False):
     if repos is None:
@@ -271,19 +293,22 @@ def deactivate_requirement(requirements, repos=None, interactive=False,
                 if package.distribution in requirement:
                     deactivated = True
                     if interactive:
-                        response = query_user("Deactivate '%s' from %s (y/n)? " % (package.name,
-                            package.repository.location), default="y")
+                        response = query_user("Deactivate '%s' from %s (y/n)? "
+                                              % (package.name,
+                                                 package.repository.location),
+                                              default="y")
                         if not response:
-                            continue                
+                            continue
                     package.deactivate(dry_run=dry_run)
         if not deactivated:
             warning("Found no requirement which matches %s" % requirement)
+
 
 def list_installed(interactive=True, term_width=0):
     repos = get_local_repos()
     for repo in repos:
         repo.build_package_list()
-        
+
         # Pretty print repo information
         print repo.location
         print "="*len(repo.location)
@@ -298,9 +323,17 @@ def list_installed(interactive=True, term_width=0):
             print "No packages installed"
         print
 
+
 def setup_parser():
-    parser = OptionParser()
-    
+    description = """\
+Utility for managing packages in the site-packages directory.
+The command needs to be on of the following: install, upgrade, remove, list
+"""
+
+    parser = OptionParser(usage="usage: enpgk command [options]",
+                          description=description,
+                          version="Enstaller version %s" % __version__)
+
     term_width = 80
     try:
         import fcntl, termios, struct
@@ -309,12 +342,12 @@ def setup_parser():
         term_width = struct.unpack('hh', data)[1]
     except ImportError:
         pass
-    
+
     parser.set_defaults(interactive=False, logging=INFO, dry_run=False,
         term_width=term_width, remote_html=[PYPI_REPO],
         find_links=[ENTHOUGHT_REPO], remote_xmlrpc=[],
         allow_unstable=False, proxy="")
-    
+
     parser.add_option("-i", "--interactive", action="store_true",
         dest="interactive", help="prompt user for choices")
     parser.add_option("-v", "--verbose", action="store_const", const=DEBUG,
@@ -337,26 +370,33 @@ def setup_parser():
         dest="proxy", help="use a proxy for downloads")
     return parser
 
+
 def main():
     # get our options
     parser = setup_parser()
     options, args = parser.parse_args()
 
     if len(args) < 1:
-        raise ValueError, "Must call enpkg with one of 'install', 'upgrade', 'remove', or 'list'"
-    
+        parser.error("Must call enpkg with one of 'install', 'upgrade', "
+                     "'remove', or 'list', see -h for more details")
+
     # set up logging
     basicConfig(level=options.logging, format="%(message)s")
-    
+
     # warn the user if the detected platform may not be supported
     supported = ["windows", "macosx", "debian", "rhel", "suse", "ubuntu"]
-    
+
     if PLAT not in supported or PLAT_VER == "":
         msg = """
-        Warning: There may not be an Enthought repository for platform "%s" "%s".
-        Check http://code.enthought.com/enstaller/eggs for the available platforms.
+        Warning: There may not be an Enthought repository for
+        platform "%s" "%s".
+        Check
+
+            http://code.enthought.com/enstaller/eggs
+
+        for the available platforms.
         """ % (PLAT, PLAT_VER)
-    
+
         warning(msg)
 
     if options.allow_unstable:
@@ -364,7 +404,7 @@ def main():
     remote_repos=[HTMLRepository(arg) for arg in options.remote_html]
     # XXX hack!  Should make the find_packages part of the Repository object
     remote_repos[0].environment.add_find_links(options.find_links)
-    
+
     try:
         # try to set up a proxy server, either from options or environment
         # this makes urllib2 calls do the right thing
@@ -372,7 +412,7 @@ def main():
     except ValueError, e:
         error('Proxy configuration error: %s' % e)
         sys.exit(2)
-    
+
     command = args[0]
     args = args[1:]
     if command == "install":
@@ -388,17 +428,18 @@ def main():
     elif sys.argv[1] == "activate":
         install_requirement([Requirement.parse(arg) for arg in args],
             remote_repos=[], interactive=options.interactive,
-            dry_run=options.dry_run, term_width=options.term_width)        
+            dry_run=options.dry_run, term_width=options.term_width)
     elif sys.argv[1] == "deactivate":
         deactivate_requirement([Requirement.parse(arg) for arg in args],
-            interactive=options.interactive, dry_run=options.dry_run)        
+            interactive=options.interactive, dry_run=options.dry_run)
     elif command == "remove":
         remove_requirement([Requirement.parse(arg) for arg in args],
             interactive=options.interactive, dry_run=options.dry_run)
     elif command == "list":
         list_installed(interactive=options.interactive,
             term_width=options.term_width)
-        
+
+
 if __name__ == "__main__":
     main()
     sys.exit()
