e6e535a3935df5373a8e8cfc691f7afd53331295
ischnell <ischnell@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2009-04-29 23:37:31 +0000

    added md5sum of Enstaller 3.0.9 eggs released to PyPI

--- ez_enstaller.py
+++ ez_enstaller.py
@@ -23,7 +23,7 @@ except ImportError: from md5 import md5
 
 
 # Setup some global vars
-DEFAULT_VERSION = "3.0.8"
+DEFAULT_VERSION = "3.0.9"
 DEFAULT_URL = "http://pypi.python.org/packages/%s/E/Enstaller/" % sys.version[:3]
 md5_data = {
     'Enstaller-3.0.4-py2.4.egg': '6110e9daf82a72b990cf22cacd63db7c',
@@ -41,6 +41,9 @@ md5_data = {
     'Enstaller-3.0.8-py2.4.egg': 'daf87ca3d5da76f42c422134558e9b84',
     'Enstaller-3.0.8-py2.5.egg': '469bfec4ba78d09df5ea9877f3631760',
     'Enstaller-3.0.8-py2.6.egg': '5bbe907981fbb30a892ecc3766dbdbf3',
+    'Enstaller-3.0.9-py2.4.egg': 'ac98db616e6375ee1863fa5484807706',
+    'Enstaller-3.0.9-py2.5.egg': 'ec48dba0c10d58455272d9ebf6d05c07',
+    'Enstaller-3.0.9-py2.6.egg': '2e5dadf0f41a311f611cd7f60b24ee6c',
 }
 
 
