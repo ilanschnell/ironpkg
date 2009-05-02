9746e0665e45a05f3502288ad84354e3197bb2ca
ischnell <ischnell@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2009-05-02 02:05:13 +0000

    added function to create proxy scripts on Windows

--- egginst/scripts.py
+++ egginst/scripts.py
@@ -1,10 +1,11 @@
 import os
 import sys
 import shutil
-from os.path import basename, join
+from os.path import basename, exists, join
 
 on_Win = sys.platform.startswith('win')
 BIN_DIR = join(sys.prefix, 'Scripts' if on_Win else 'bin')
+CLI_EXE = join(BIN_DIR, 'egginst.exe')
 
 
 def get_python():
@@ -14,12 +15,48 @@ def get_python():
         return sys.executable
 
 
+def unlink(fpath):
+    if exists(dst):
+        print "Warning: %r already exists, unlinking" % dst
+        os.unlink(dst)
+
+
+def create_proxy(src):
+    print "Creating proxy executable to: %r" % src
+    assert src.endswith('.exe')
+
+    dst_name = basename(src)
+    if dst_name.startswith('epd-'):
+        dst_name = dst_name[4:]
+
+    dst = join(BIN_DIR, dst_name)
+    unlink(dst)
+    shutil.copyfile(CLI_EXE, dst)
+
+    dst_script = dst[:-4] + '-script.py'
+    unlink(dst_script)
+    fo = open(join(BIN_DIR, dst_script), 'w')
+    fo.write('''\
+#!%(python)s
+import sys
+import subprocess
+
+src = %(src)r
+
+sys.exit(subprocess.call([src] + sys.argv[1:]))
+''' % dict(python=get_python(), src=src))
+    fo.close()
+
+    return dst, dst_script
+
+
 def write_script(fpath, entry_pt, egg_name):
     print 'Creating script', fpath
 
     assert entry_pt.count(':') == 1
     module, func = entry_pt.strip().split(':')
 
+    unlink(fpath)
     fo = open(fpath, 'w')
     fo.write('''\
 #!%(python)s
@@ -39,7 +76,8 @@ def create(egg, conf):
         fname = name
         if on_Win:
             exe = join(BIN_DIR, name + '.exe')
-            shutil.copyfile(join(BIN_DIR, 'egginst.exe'), exe)
+            unlink(exe)
+            shutil.copyfile(CLI_EXE, exe)
             egg.files.append(exe)
 
             fname += '-script.py'
