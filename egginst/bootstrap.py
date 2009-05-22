4efa981f0b29fb519e686c95a217151c4e4a4429
ischnell <ischnell@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2009-05-22 00:07:52 +0000

    changed function to return exit code, instead of just exiting

--- egginst/bootstrap.py
+++ egginst/bootstrap.py
@@ -4,4 +4,5 @@ import sys
 def main():
     # For now, just use the bootstrap setuptools provides
     from setuptools.command.easy_install import bootstrap
-    sys.exit(bootstrap())
+
+    return bootstrap()
