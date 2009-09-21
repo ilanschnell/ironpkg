7b3cd23bbc1afd70cdbd09ad70adaa6fb607d454
ischnell <ischnell@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2009-09-21 18:31:44 +0000

    simplified build script, making it independent of special tools

--- build.sh
+++ build.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
-EGG=Enstaller-4.0.1-1.egg
+VER=4.1.0
 
+
+SPEC=Enstaller.egg-info/spec
+mkdir -p $SPEC
+sed -e "s/XXX/$VER/" <<EOF >$SPEC/depend
+metadata_version = '1.1'
+name = 'Enstaller'
+version = 'XXX'
+build = 1
+
+arch = None
+platform = None
+osdist = None
+python = '2.5'
+packages = []
+EOF
+
+
+EGG=dist/Enstaller-$VER-1.egg
 rm -rf build dist
 python setup.py bdist_egg
-cp depend.txt depend
-mv dist/Enstaller-*.egg dist/$EGG
-egginfo -u spec/depend dist/$EGG
-egginfo --sd dist/$EGG
-repo-upload --force --no-confirm dist/$EGG
+mv dist/Enstaller-*.egg $EGG
+
+# egginfo --sd $EGG
+# repo-upload --force --no-confirm $EGG
