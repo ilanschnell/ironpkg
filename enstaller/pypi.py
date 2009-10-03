a7333351fdcdda60958e32f20c30ea785d95e4a0
ischnell <ischnell@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2009-10-03 04:24:44 +0000

    added code to test the PyPI-XMLRPC interface, removed unused module

--- enstaller/pypi.py
+++ enstaller/pypi.py
@@ -1,4 +1,27 @@
 # this test code is not imported anywhere
+import xmlrpclib
+import sys
+ 
+pypi = xmlrpclib.ServerProxy('http://python.org/pypi')
+for hit in pypi.search(dict(name=sys.argv[1])):
+    print '''\
+NAME: %(name)s %(version)s
+SUMM: %(summary)s''' % hit
+    urls = pypi.package_urls(hit['name'], hit['version'])
+    if urls:
+        for url in urls:
+            #print repr(url)
+            print '''\
+%(url)s
+\t filename: %(filename)s (%(size)i bytes)
+\t pkgtype : %(packagetype)s
+\t py-vers : %(python_version)s''' % url
+
+    print
+
+
+
+sys.exit()
 
 import re
 import StringIO
