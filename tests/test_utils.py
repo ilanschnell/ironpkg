dd196b838f8a263b48d621dd1bcaed522f2a554b
ischnell <ischnell@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2010-02-13 19:39:33 +0000

    ENH: adding tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,6 +9,7 @@ class TestUtils(unittest.TestCase):
     def test_canonical(self):
         for name, cname in [
             ('NumPy', 'numpy'),
+            ('MySql-python', 'mysql_python'),
             ('Python-dateutil', 'python_dateutil'),
             ('Tables', 'pytables'),
             ]:
@@ -17,22 +18,19 @@ class TestUtils(unittest.TestCase):
     def test_cname_fn(self):
         self.assertEqual(cname_fn('VTK-5.4.2-1.egg'), 'vtk')
 
-    def test_comparable_version1(self):
-        versions = ['1.0.4', '1.2.1', '1.3.0b1', '1.3.0', '1.3.10',
-                    '1.3.11.dev7', '1.3.11.dev12', '1.3.11.dev111',
-                    '1.3.11',
-                    '1.4.0.dev7749', '1.4.0rc1', '1.4.0rc2', '1.4.0']
-        org = list(versions)
-        random.shuffle(versions)
-        versions.sort(key=comparable_version)
-        self.assertEqual(versions, org)
-
-    def test_comparable_version2(self):
-        versions = ['2008j', '2008k', '2009b', '2009h']
-        org = list(versions)
-        random.shuffle(versions)
-        versions.sort(key=comparable_version)
-        self.assertEqual(versions, org)
+    def test_comparable_version(self):
+        for versions in (
+            ['1.0.4', '1.2.1', '1.3.0b1', '1.3.0', '1.3.10',
+             '1.3.11.dev7', '1.3.11.dev12', '1.3.11.dev111',
+             '1.3.11', '1.3.143',
+             '1.4.0.dev7749', '1.4.0rc1', '1.4.0rc2', '1.4.0'],
+            ['2008j', '2008k', '2009b', '2009h', '2010b'],
+            ['0.99', '1.0a2', '1.0b1', '1.0rc1', '1.0', '1.0.1'],
+            ):
+            org = list(versions)
+            random.shuffle(versions)
+            versions.sort(key=comparable_version)
+            self.assertEqual(versions, org)
 
 
 if __name__ == '__main__':
