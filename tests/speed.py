ab087a484c48bbe2ba536092c7ce4f1d4c9da9ad
ischnell <ischnell@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2010-05-13 05:24:49 +0000

    allow test to be run on other platforms

--- tests/speed.py
+++ tests/speed.py
@@ -13,7 +13,7 @@ for fn in ['index-5.1.txt', 'index-5.0.txt']:
 requirement.PY_VER = '2.5'
 h = hashlib.new('md5')
 # This is quite expensive and takes about 2.3 seconds on my 2GHz MacBock
-for dist in c.install_order(Req('epd 5.1.0'), True):
+for dist in c.install_order(Req('epd 5.1.0')):
     for d in c.install_order(dist_as_req(dist)):
-        h.update(d)
-assert h.hexdigest() == '6c4c583bdb87634729c55bf23140f36d'
+        h.update(filename_dist(d))
+assert h.hexdigest() == '1d0c258f0e5d73d1a386278f90db2d34'
