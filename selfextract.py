9fb53d22f61474a7d35a024fdd146ea27a2b5ee6
Ilan Schnell <ischnell@enthought.com>
2011-02-08 22:52:04 -0600

    updating work on self installing script

--- selfextract.py
+++ selfextract.py
@@ -1,19 +1,53 @@
+"""\
+Self installs IronPkg into the current IronPython environment.
+
+Options:
+  --install
+"""
+import os
 import sys
+import tempfile
 import zipfile
-from os.path import join
-from optparse import OptionParser
+from os.path import dirname, isdir, join
 
 
 eggdata = "EGGDATA"
 
-def cli():
-    egg_path = join("EGGNAME")
+
+def unzip(zip_file, dir_path):
+    """Unzip the zip_file into the directory dir_path."""
+    z = zipfile.ZipFile(zip_file)
+    for name in z.namelist():
+        if name.endswith('/'):
+            continue
+        path = join(dir_path, *name.split('/'))
+        if not isdir(dirname(path)):
+            os.makedirs(dirname(path))
+        fo = open(path, 'wb')
+        fo.write(z.read(name))
+        fo.close()
+    z.close()
+
+
+def self_install():
+    tmp_dir = tempfile.mkdtemp()
+    egg_path = join(tmp_dir, "EGGNAME")
     fo = open(egg_path, 'wb')
     fo.write(eggdata)
     fo.close()
-    sys.path.insert(0, egg_path)
-    from egginst.bootstrap import main
-    main()
+    unzip(egg_path, tmp_dir)
+    sys.path.insert(0, tmp_dir)
+    import egginst
+    print "Bootstrapping:", egg_path
+    ei = egginst.EggInst(egg_path)
+    ei.install()
+
+
+def cli():
+    if '--install' in sys.argv:
+        self_install()
+    else:
+        print __doc__
 
 
 if __name__ == '__main__':
